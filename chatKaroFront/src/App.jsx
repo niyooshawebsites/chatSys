089d1b2a90c8dceb7a664c0aa867e321b6ffe683
niyooshawebsites <niyooshawebsites@gmail.com>
2024-05-25 14:17:57 +0530

    chat page protected v1

--- chatKaroFront/src/App.jsx
+++ chatKaroFront/src/App.jsx
@@ -5,11 +5,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chat from "./components/Private/chat/Chat";
 import { Provider } from "react-redux";
 import centralStore from "./store/CentralStore";
-import { Outlet, useNavigate } from "react-router-dom";
 import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
 
 function App() {
-  const navigate = useNavigate();
   const authToken = sessionStorage.getItem("authToken");
   return (
     <>
@@ -18,7 +16,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Signup />} />
             <Route path="/login" element={<Login />} />
-            <Route element={<ProtectedRoute authToken={authToken} />} />
+            <Route element={<ProtectedRoute authToken={authToken} />}>
+              <Route path="/chat" element={<Chat />} />
+            </Route>
           </Routes>
         </BrowserRouter>
       </Provider>
