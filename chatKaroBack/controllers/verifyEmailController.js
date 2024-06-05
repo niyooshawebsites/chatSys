7384f70aa19afb08f753cd98ff0ec33c76bee4d4
niyooshawebsites <niyooshawebsites@gmail.com>
2024-06-05 15:24:02 +0530

    restructure verifyEmailController

--- chatKaroBack/controllers/verifyEmailController.js
+++ chatKaroBack/controllers/verifyEmailController.js
@@ -5,60 +5,59 @@ const bcrypt = require("bcrypt");
 
 const verifyEmailController = async (req, res) => {
   try {
-    // getting opt and owner from the body
     const { otp, owner } = req.body;
-    // check if the otp and owner are present
     if (!otp || !owner) {
       return res.status(400).json({
         success: false,
         message: "No OTP or User ID",
       });
-    } else if (!isValidObjectId(owner)) {
-      return res.status(404).json({
-        success: false,
-        message: `Invalid User ID`,
-      });
-    } else if (isValidObjectId(owner)) {
-      // find the user from the database
-      const user = await userModel.findOne({ _id: owner });
-      if (!user) {
+    }
+
+    if (otp && owner) {
+      if (!isValidObjectId(owner)) {
         return res.status(404).json({
           success: false,
-          message: `User not found`,
+          message: `Invalid User ID`,
         });
-      } else if (user) {
-        // Getting the verification Token Object from the database
+      } else if (isValidObjectId(owner)) {
         const verificationTokenObject = await verificationModel.findOne({
-          owner: owner,
+          owner,
         });
+
         if (!verificationTokenObject) {
           return res.status(404).json({
             success: false,
             message: `Token not found`,
           });
-        } else {
-          const isMatched = bcrypt.compare(otp, verificationTokenObject.otp);
-          if (isMatched) {
-            user.isVerified = true;
-            await verificationModel.findByIdAndDelete({
-              owner: owner,
-            });
-
-            return res.status(200).json({
-              success: true,
-              message: "Email verified successfully!",
-            });
-          } else {
+        } else if (verificationTokenObject) {
+          const isTokenMatched = await bcrypt.compare(
+            otp,
+            verificationTokenObject.otp
+          );
+          if (!isTokenMatched) {
             return res.status(404).json({
               success: false,
               message: "Invalid OTP",
+            });
+          } else if (isTokenMatched) {
+            const user = await userModel.findOne({ _id: owner });
+            (user.isVerified = true),
+              await verificationModel.findByIdAndDelete({
+                _id: owner,
+              });
+            return res.status(200).json({
+              success: true,
+              message: "Email verified successfully!",
             });
           }
         }
       }
     }
   } catch (err) {
-    console.log(err);
+    return res.status(500).send({
+      success: false,
+      message: err,
+    });
   }
 };
 
